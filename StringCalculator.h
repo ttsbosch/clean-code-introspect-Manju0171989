d9f9ea34dd8d588e0e4d91796e4e34faa3681de3
Manju0171989 <manju017.1989@gmail.com>
2024-06-27 17:00:50 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -7,7 +7,7 @@ class StringCalculator
      std::vector<int> ExtractedIntInputData;
      bool isValidInput(std::string input);
      std::vector<int> extractNumbers(std::string input);
-     std::pair<std::string, std::string> parseCustomDelimiter(const std::string& numbers)
+     std::pair<std::string, std::string> parseCustomDelimiter(const std::string& numbers);
     public:
        int add(std::string input);
        StringCalculator();
