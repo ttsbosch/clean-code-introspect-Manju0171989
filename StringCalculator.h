f7ca5e269b2445a9adffc6184bcd894baa6796e0
Manju0171989 <manju017.1989@gmail.com>
2024-06-27 15:03:07 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 class StringCalculator
 {
+     bool validateInput(std::string input);
+     void extractInputData(std::string input,std::vector<int>&inputData);
     public:
        int add(std::string input);
 };
